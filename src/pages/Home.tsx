fa111b4bdf174f96c87b33297a2480957a0eab54
WylderM <wyldermenezes@gmail.com>
2020-03-12 15:56:04 -0300

    list api

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,20 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import React from 'react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Home.css';
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  IonItem,
+} from "@ionic/react";
+import React from "react";
+import ExploreContainer from "../components/ExploreContainer";
+import "./Home.css";
+import { useQuery } from "@apollo/react-hooks";
+import { LAUNCHES_PAST_QUERY } from "../graphql/lauches";
 
 const Home: React.FC = () => {
+  const { data, loading } = useQuery(LAUNCHES_PAST_QUERY);
+
   return (
     <IonPage>
       <IonHeader>
@@ -18,6 +29,16 @@ const Home: React.FC = () => {
           </IonToolbar>
         </IonHeader>
         <ExploreContainer />
+        {loading ? (
+          <p> loading... </p>
+        ) : (
+          data &&
+          data.launchesPast.map((launch: any) => (
+            <IonItem key={launch.id}>
+              {launch.mission_name} | {launch.rocket.rocket_name}
+            </IonItem>
+          ))
+        )}
       </IonContent>
     </IonPage>
   );
